e40d39e0c377628e5e972dbd3d743dfca1a9a583
Keich.Park <svclaw2000@gmail.com>
2022-03-26 01:59:27 +0900

    feat: DAM-8 Result 확장 함수 추가

---
+++ domain/src/main/java/com/khnsoft/damta/domain/common/ResultExtentions.kt
@@ -0,0 +1,28 @@
+package com.khnsoft.damta.domain.common
+
+inline fun <T, R> Result<T>.flatMap(
+    transform: (T) -> Result<R>
+): Result<R> {
+    return when (val exception = exceptionOrNull()) {
+        null -> transform(getOrThrow())
+        else -> Result.failure(exception)
+    }
+}
+
+inline fun <T> Result<T>.errorMap(
+    transform: (Throwable) -> Exception
+): Result<T> {
+    return when (val exception = exceptionOrNull()) {
+        null -> this
+        else -> Result.failure(transform(exception))
+    }
+}
+
+inline fun <T> Result<T>.handleWith(
+    transform: (Throwable) -> Result<T>
+): Result<T> {
+    return when (val exception = exceptionOrNull()) {
+        null -> this
+        else -> transform(exception)
+    }
+}
