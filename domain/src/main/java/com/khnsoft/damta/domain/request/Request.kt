157599aff78fe24979af7fd08b81599af1b0d1ee
Keich.Park <svclaw2000@gmail.com>
2022-03-26 01:12:31 +0900

    feat: DAM-8 UseCase 인터페이스 추가

---
+++ domain/src/main/java/com/khnsoft/damta/domain/request/Request.kt
@@ -0,0 +1,3 @@
+package com.khnsoft.damta.domain.request
+
+interface Request
---
+++ domain/src/main/java/com/khnsoft/damta/domain/usecase/ResultUseCase.kt
@@ -0,0 +1,9 @@
+package com.khnsoft.damta.domain.usecase
+
+import com.khnsoft.damta.domain.request.Request
+import com.khnsoft.damta.domain.response.Response
+
+interface ResultUseCase<R: Request, T: Response> {
+
+    suspend operator fun invoke(request: R): Result<T>
+}
