2767254d493097f1037b21c613d3ab349b90f255
Keich.Park <svclaw2000@gmail.com>
2022-03-26 17:44:44 +0900

    feat: DAM-8 사용자 local source 추가

---
+++ local/src/main/java/com/khnsoft/damta/local/di/LocalDataSourceModule.kt
@@ -0,0 +1,18 @@
+package com.khnsoft.damta.local.di
+
+import com.khnsoft.damta.data.source.UserLocalDataSource
+import com.khnsoft.damta.local.source.UserLocalDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal abstract class LocalDataSourceModule {
+
+    @Binds
+    abstract fun bindUserLocalDataSource(
+        source: UserLocalDataSourceImpl
+    ): UserLocalDataSource
+}
