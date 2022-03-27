b1bf2638a1f83c34ec84c8eaf294b25c9fe17e6a
Keich.Park <svclaw2000@gmail.com>
2022-03-27 16:27:09 +0900

    feat: DAM-10 Repository 싱글톤으로 변경

--- data/src/main/java/com/khnsoft/damta/data/repository/UserRepositoryImpl.kt
+++ data/src/main/java/com/khnsoft/damta/data/repository/UserRepositoryImpl.kt
@@ -11,7 +11,9 @@ import com.khnsoft.damta.domain.model.User
 import com.khnsoft.damta.domain.repository.UserRepository
 import java.time.LocalDate
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 internal class UserRepositoryImpl @Inject constructor(
     private val local: UserLocalDataSource
 ) : UserRepository {
