5d0eaef86ce24026e05132712855d306eeab7b9a
Keich.Park <svclaw2000@gmail.com>
2022-03-27 14:50:54 +0900

    feat: DAM-9 로그인 테스트 추가

---
+++ domain/src/test/java/com/khnsoft/damta/domain/usecase/SignInUseCaseTest.kt
@@ -0,0 +1,62 @@
+package com.khnsoft.damta.domain.usecase
+
+import com.khnsoft.damta.domain.common.HashGenerator
+import com.khnsoft.damta.domain.error.UserError
+import com.khnsoft.damta.domain.repository.UserRepository
+import com.khnsoft.damta.domain.request.SignInRequest
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class SignInUseCaseTest {
+
+    @MockK private lateinit var userRepository: UserRepository
+    @MockK private lateinit var hashGenerator: HashGenerator
+    private lateinit var useCase: SignInUseCase
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        useCase = SignInUseCase(
+            userRepository = userRepository,
+            hashGenerator = hashGenerator
+        )
+    }
+
+    @Test
+    fun invoke_validAccount_isSuccess(): Unit = runBlocking {
+        every {
+            hashGenerator.hashPasswordWithSalt("password", "username")
+        } returns "password with salt"
+
+        coEvery {
+            userRepository.signIn("username", "password with salt")
+        } returns Result.success(Unit)
+
+        val result = useCase(SignInRequest("username", "password"))
+
+        assertTrue(result.isSuccess)
+    }
+
+    @Test
+    fun invoke_invalidAccount_isFailure(): Unit = runBlocking {
+        every {
+            hashGenerator.hashPasswordWithSalt("password", "username")
+        } returns "password with salt"
+
+        coEvery {
+            userRepository.signIn("username", "password with salt")
+        } returns Result.failure(UserError.InvalidUsernameOrPassword())
+
+        val result = useCase(SignInRequest("username", "password"))
+
+        assertTrue(result.isFailure)
+        assertEquals(UserError.InvalidUsernameOrPassword(), result.exceptionOrNull())
+    }
+}
