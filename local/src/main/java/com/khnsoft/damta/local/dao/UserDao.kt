1f4a0ae2e8f818dbfb6873646b09b0871bd93d55
Keich.Park <svclaw2000@gmail.com>
2022-03-27 15:44:46 +0900

    feat: DAM-10 사용자 정보 수정 data source 추가

--- local/src/main/java/com/khnsoft/damta/local/dao/UserDao.kt
+++ local/src/main/java/com/khnsoft/damta/local/dao/UserDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.khnsoft.damta.local.model.UserDto
+import java.time.LocalDate
 
 @Dao
 internal interface UserDao {
@@ -14,4 +15,17 @@ internal interface UserDao {
 
     @Query("SELECT * FROM User WHERE username=:username AND password=:password")
     suspend fun fetchUserByUsernameAndPassword(username: String, password: String): UserDto?
+
+    @Query("UPDATE User SET email=:email, nickname=:nickname, birthday=:birthday WHERE id=:userId")
+    suspend fun updateUser(
+        userId: Int,
+        email: String,
+        nickname: String,
+        birthday: LocalDate
+    )
+
+    @Query("SELECT * FROM User WHERE id=:userId")
+    suspend fun fetchUserById(
+        userId: Int,
+    ): UserDto?
 }
