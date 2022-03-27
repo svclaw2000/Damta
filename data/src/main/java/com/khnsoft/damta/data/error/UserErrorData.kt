8c1090a69e696182cd9b670f71a7ec8dd3cd95bf
Keich.Park <svclaw2000@gmail.com>
2022-03-27 15:48:58 +0900

    feat: DAM-10 ErrorData 추가

--- data/src/main/java/com/khnsoft/damta/data/error/UserErrorData.kt
+++ data/src/main/java/com/khnsoft/damta/data/error/UserErrorData.kt
@@ -3,7 +3,7 @@ package com.khnsoft.damta.data.error
 sealed class UserErrorData(
     override val message: String? = null,
     override val cause: Throwable? = null
-) : Exception(message, cause) {
+) : ErrorData(message, cause) {
 
     data class DuplicatedUsername(
         override val message: String? = null,
