91678ee311516324d215a72a8e5844017160c919
Keich.Park <svclaw2000@gmail.com>
2022-03-27 15:32:10 +0900

    feat: DAM-10 사용자 정보 수정 repository 추가

--- data/src/main/java/com/khnsoft/damta/data/source/UserLocalDataSource.kt
+++ data/src/main/java/com/khnsoft/damta/data/source/UserLocalDataSource.kt
@@ -1,10 +1,20 @@
 package com.khnsoft.damta.data.source
 
 import com.khnsoft.damta.data.model.UserData
+import java.time.LocalDate
 
 interface UserLocalDataSource {
 
     suspend fun signUp(user: UserData, password: String): Result<Unit>
 
     suspend fun signIn(username: String, password: String): Result<Int>
+
+    suspend fun updateUser(
+        userId: Int,
+        email: String,
+        nickname: String,
+        birthday: LocalDate
+    ): Result<Unit>
+
+    suspend fun fetchUserData(userId: Int): Result<UserData>
 }
