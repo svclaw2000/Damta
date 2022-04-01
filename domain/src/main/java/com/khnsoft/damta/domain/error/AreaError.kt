ae8d6187750b1e3b043a6297fcc39b8d4544af8e
Keich.Park <svclaw2000@gmail.com>
2022-04-02 02:04:09 +0900

    feat: DAM-11 흡연구역 등록 UseCase 추가

---
+++ domain/src/main/java/com/khnsoft/damta/domain/error/AreaError.kt
@@ -0,0 +1,9 @@
+package com.khnsoft.damta.domain.error
+
+sealed class AreaError(
+    override val message: String? = null,
+    override val cause: Throwable? = null
+) : Exception(message, cause) {
+
+    object EmptyName : AreaError()
+}
---
+++ domain/src/main/java/com/khnsoft/damta/domain/response/area/AddAreaResponse.kt
@@ -0,0 +1,7 @@
+package com.khnsoft.damta.domain.response.area
+
+import com.khnsoft.damta.domain.response.Response
+
+data class AddAreaResponse(
+    val areaId: Int
+) : Response
