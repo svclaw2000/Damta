7ba4b1f518c7bb6a948c7ae6c7e3387aef72b2d1
Keich.Park <svclaw2000@gmail.com>
2022-04-09 14:07:17 +0900

    feat: DAM-13 장소 검색 API 추가

--- remote/src/main/java/com/khnsoft/damta/remote/api/KakaoApiService.kt
+++ remote/src/main/java/com/khnsoft/damta/remote/api/KakaoApiService.kt
@@ -1,6 +1,23 @@
 package com.khnsoft.damta.remote.api
 
+import com.khnsoft.damta.remote.model.SearchAddressResponse
+import com.khnsoft.damta.remote.model.SearchPlaceResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 internal interface KakaoApiService {
 
+    @GET("/v2/local/search/address.JSON")
+    suspend fun searchAddress(
+        @Query("query") keyword: String,
+        @Query("page") page: Int = 1,
+        @Query("size") size: Int = 10
+    ): SearchAddressResponse
 
+    @GET("/v2/local/search/keyword.JSON")
+    suspend fun searchPlace(
+        @Query("query") keyword: String,
+        @Query("page") page: Int = 1,
+        @Query("size") size: Int = 10
+    ): SearchPlaceResponse
 }
